be3cb520c2a624809f67f84c49c64d82a303e086
KauBryy <aubrypierre69@gmail.com>
2025-12-19 20:08:53 +0100

    Changement texte badge hero: EXCLUSIVITÉ ANDROID -> APPLICATION GRATUITE

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -63,7 +63,7 @@ const Index = () => {
                   <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-brand opacity-75"></span>
                   <span className="relative inline-flex rounded-full h-2 w-2 bg-brand"></span>
                 </span>
-                <span>EXCLUSIVITÉ ANDROID</span>
+                <span>APPLICATION GRATUITE</span>
               </div>
               <h1 className="text-6xl md:text-7xl font-extrabold leading-[1.1] mb-6">
                 Maîtrisez votre{" "}
